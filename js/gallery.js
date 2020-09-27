63335b74bc93ceaacc3d90fbef683b2505812551
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-09-27 21:03:15 +0300

    Додає
     3. Модальне вікно. Відкриття при кліку на зображення.
    4. Підміна значення атрибута src елемента img.lightbox__image.
    5. Закриття модального вікна при натисканні на кнопку button[data - action= "close-modal"]
    6. Очищення значення атрибута src елемента img.lightbox__image.

--- js/gallery.js
+++ js/gallery.js
@@ -3,6 +3,9 @@ import images from '../gallery-items.js';
 
 const refs = {
   gallery: document.querySelector('.js-gallery'),
+  modal: document.querySelector('.js-lightbox'),
+  modalIage: document.querySelector('.lightbox__image'),
+  modalCloseBtn: document.querySelector('button[data-action="close-lightbox"]'),
 };
 
 const galleryElementMarkup = createImagesCollection(images);
@@ -27,14 +30,41 @@ function createImagesCollection(obj) {
     })
     .join('');
 }
-// додає розмітку в html
+
 refs.gallery.insertAdjacentHTML('beforeend', galleryElementMarkup);
 
 // 2. Реалізація делегування на галереї ul.js-gallery і отримання url великого зображення.
-// 3. Модальне вікно. Відкриття при кліку на зображення.
-// 4. Підміна значення атрибута src елемента img.lightbox__image.
+refs.gallery.addEventListener('click', onImageClick);
+
+function onImageClick(e) {
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  e.preventDefault();
+
+  const originalImgUrl = e.target.dataset.source;
+  //   console.log(e.target.dataset.source);
+  const altImg = e.target.alt;
+  //   console.log(e.target.alt);
+
+  // 3. Модальне вікно. Відкриття при кліку на зображення.
+  refs.modal.classList.add('is-open');
+
+  // 4. Підміна значення атрибута src елемента img.lightbox__image.
+  refs.modalIage.src = originalImgUrl;
+  refs.modalIage.alt = altImg;
+}
+
 // 5. Закриття модального вікна при натисканні на кнопку button[data - action= "close-modal"].
-// 6. Очищення значення атрибута src елемента img.lightbox__image.Це необхідно   для того, щоб при наступному відкритті модального вікна, поки вантажиться   зображення, ми не бачили попереднє.
+refs.modalCloseBtn.addEventListener('click', onCloseBtnClick);
+
+function onCloseBtnClick() {
+  refs.modal.classList.remove('is-open');
+  // 6. Очищення значення атрибута src елемента img.lightbox__image.Це необхідно   для того, щоб при наступному відкритті модального вікна, поки вантажиться   зображення, ми не бачили попереднє.
+  refs.modalIage.src = '';
+  refs.modalIage.alt = '';
+}
 
 // Додатково
 // 7. Закриття модального вікна при натисканні на div.lightbox__overlay.
